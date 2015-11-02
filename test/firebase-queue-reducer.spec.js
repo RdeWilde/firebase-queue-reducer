849e049ff295b981b3a084dc92affaf5ac7bdced
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-11-02 12:09:39 +0100

    basic testing setup

---
+++ test/firebase-queue-reducer.spec.js
@@ -0,0 +1,10 @@
+
+var FirebaseQueueReducer = require('../index');
+
+describe('FirebaseQueueReducer', function() {
+
+    it('should work', function() {
+        new FirebaseQueueReducer();
+    });
+
+});
