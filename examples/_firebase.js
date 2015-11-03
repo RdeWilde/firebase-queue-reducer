3cae3666870007914f98482672fce724fe816492
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-11-03 13:26:02 +0100

    remove personal firebase url

--- examples/_firebase.js
+++ examples/_firebase.js
@@ -1,5 +1,4 @@
 var Firebase = require('firebase');
-// var fb = new Firebase('--YourFirebaseInstance--.firebaseio.com');
-var fb = new Firebase('peg-dev.firebaseio.com');
+var fb = new Firebase('--YourFirebaseInstance--.firebaseio.com');
 
 module.exports = fb;
